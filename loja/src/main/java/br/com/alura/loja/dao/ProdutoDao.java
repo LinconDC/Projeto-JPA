a874f3639e9aa38c9c78c1fc6fdcf62064853b02
Lincon Martins <linconmartins18@gmail.com>
2022-07-28 09:21:35 -0300

    search with jpa

--- loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,12 +1,15 @@
 package br.com.alura.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+
 import br.com.alura.loja.modelo.Produto;
 
 public class ProdutoDao {
 
 	private EntityManager em;
-	
+
 	public ProdutoDao() {
 	}
 
@@ -16,5 +19,14 @@ public class ProdutoDao {
 
 	public void cadastrar(Produto produto) {
 		this.em.persist(produto);
+	}
+
+	public Produto buscarPorId(Long id) {
+		return em.find(Produto.class, id);
+	}
+
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
 	}
 }
