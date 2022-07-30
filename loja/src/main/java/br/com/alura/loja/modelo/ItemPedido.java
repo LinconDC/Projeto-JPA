aaac1340f1aa19c0c8eb7a9903942ca8605fd9a6
Lincon Martins <linconmartins18@gmail.com>
2022-07-30 07:51:31 -0300

    Cascade

--- loja/src/main/java/br/com/alura/loja/modelo/ItemPedido.java
+++ loja/src/main/java/br/com/alura/loja/modelo/ItemPedido.java
@@ -2,6 +2,7 @@ package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class ItemPedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "preco_unitario")
 	private BigDecimal precoUnitario;
 	private int quantidade;
 
@@ -31,6 +33,7 @@ public class ItemPedido {
 	public ItemPedido(int quantidade, Pedido pedido, Produto produto) {
 		this.quantidade = quantidade;
 		this.pedido = pedido;
+		this.precoUnitario = produto.getPreco();
 		this.produto = produto;
 	}
 
